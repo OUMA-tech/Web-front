73234909672f6401b528775c28cbed38c42f71a4
OUMA_kk <tianyfff11@gmail.com>
2023-11-09 01:53:26 +1100

    2.1.2 finished register page and reviewed useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
 function App () {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [name, setName] = React.useState('')
+  const register = async () => {
+    console.log(email, name, password);
+    const response = await fetch('http://localhost:5005/user/auth/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        email, password, name
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      }
+    })
+    const data = await response.json();
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+    }
+    console.log(data);
+  }
   return (
-    <>Let&apos;s go!</>
+    <>
+    <h2>Register</h2>
+    Email: <input type='text' value = {email} onChange = {e => { setEmail(e.target.value) }}/><br />
+    Password: <input type='password' value = {password} onChange = {e => { setPassword(e.target.value) }}/><br />
+    Name: <input type='text' value = {name} onChange = {e => { setName(e.target.value) }}/><br />
+    <button type='button' onClick={register}>Register</button>
+    </>
   );
 }
 
